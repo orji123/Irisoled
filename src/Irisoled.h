003d5e6ef7de7764b10bc8a8476790e77025ab78
Chijindu-Orji Iseh-Ntah <chijipeters1@gmail.com>
2025-08-15 15:25:33 +0100

    Update

---
+++ src/Irisoled.h
@@ -0,0 +1,37 @@
+/***************************************************
+  Irisoled - Robotic OLED Eye Expressions Library
+  Copyright (c) 2025 Chijindu-Orji Iseh-Ntah
+
+  Licensed under the MIT License.
+  See license.txt for details.
+****************************************************/
+
+#ifndef IRISOLED_H
+#define IRISOLED_H
+
+#include <Arduino.h>
+
+namespace Irisoled {
+    extern const unsigned char angry[] PROGMEM;
+    extern const unsigned char blink_down[] PROGMEM;
+    extern const unsigned char blink_up[] PROGMEM;
+    extern const unsigned char bored[] PROGMEM;
+    extern const unsigned char despair[] PROGMEM;
+    extern const unsigned char excited[] PROGMEM;
+    extern const unsigned char focused[] PROGMEM;
+    extern const unsigned char furious[] PROGMEM;
+    extern const unsigned char happy[] PROGMEM;
+    extern const unsigned char neutral[] PROGMEM;
+    extern const unsigned char sad[] PROGMEM;
+    extern const unsigned char scared[] PROGMEM;
+    extern const unsigned char sleepy[] PROGMEM;
+    extern const unsigned char surprised[] PROGMEM;
+    extern const unsigned char worried[] PROGMEM;
+    extern const unsigned char boot[] PROGMEM;
+    extern const unsigned char left[] PROGMEM;
+    extern const unsigned char mode[] PROGMEM;
+    extern const unsigned char right[] PROGMEM;
+    extern const unsigned char warning[] PROGMEM;
+}
+
+#endif
